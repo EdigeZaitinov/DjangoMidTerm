e65726aa07c3befb240c9e3c9e328c0a833d88d2
EdigeZaitinov <edige.zzh@gmail.com>
2021-03-20 12:32:46 +0600

    commit 3

--- Mid/users/admin.py
+++ Mid/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from users.models import CustomUser
+from users.managers import CustomUserManager
+from .forms import CustomUserCreationForm
 
+@admin.register(CustomUser) #первый способ
+class UserAdmin(admin.ModelAdmin):
+    list_display=('email',)
+    list_display_links=('email',)
+    list_filter=('email',)
+    search_fields=('email',)
 # Register your models here.
